b2821ffed62c81c2803fb548df72252a9b865187
Max Moser <max.h.moser@gmail.com>
2025-02-01 12:49:22 +0100

    made a green rectangle as a snake head

--- src/main/java/org/example/Frame.java
+++ src/main/java/org/example/Frame.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame {
     int boardWidth = 600;
@@ -8,6 +9,8 @@ public class Frame {
 
     JFrame frame;
     JButton button;
+    JLabel label;
+    MyJComponent head = new MyJComponent();
 
     public Frame() {
         // Initialize JFrame
@@ -21,6 +24,22 @@ public class Frame {
         button = new JButton("hello");
         button.setBounds(100, 100, 100, 100); // Set position and size
         frame.add(button);
+
+        label = new JLabel("Score: ");
+        label.setBounds(10,10,100,10);
+        frame.add(label);
+        frame.add(head);
+        head.setBounds(20,20,100,100);
+
+
+
+    }
+    static class MyJComponent extends JComponent {
+        public void paint(Graphics g) {
+            g.setColor(Color.green);
+            g.fillRect(30, 30, 100, 100);
+        }
     }
 
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         new Frame();
-        Box [] box = new Box[16];
+
 
     }
 }
