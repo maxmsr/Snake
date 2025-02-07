10c6e36f3f89bb2aca070cb3cb36b1442fd0f15e
Max Moser <max.h.moser@gmail.com>
2025-02-07 22:43:01 +0100

    a

--- src/main/java/org/example/Frame.java
+++ src/main/java/org/example/Frame.java
@@ -2,8 +2,12 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Frame extends JFrame{
+public class Frame extends JFrame implements ActionListener {
+
+    JButton button = new JButton("Hello");
 
      Frame(){
         this.setTitle("Snake");
@@ -12,7 +16,27 @@ public class Frame extends JFrame{
         this.setSize(850,850);
         this.setVisible(true);
         this.getContentPane().setBackground(new Color(41, 101, 0));
+        //button.setBounds(100,500,100,50);
+        button.addActionListener(e -> System.out.println("H"));
+        button.setVerticalAlignment(JButton.CENTER);
+        button.setHorizontalAlignment(JButton.CENTER);
+        this.add(button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 
 
+    /*
+    @Override
+    public void actionPerformed(ActionEvent e) {
+         if(e.getSource()== button){
+             System.out.println("H");
+         }
+
+    }
+
+     */
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         Frame frame = new Frame();
+        JPanel panel = new JPanel();
+        int x = ((int) (Math.random() * 8500)/17);
+        int y = (int) (Math.random()*8500)/17;
+        panel.setBackground(Color.red);
+        panel.setBounds(x,y,85,85);
+        frame.add(panel);
  
 
 
